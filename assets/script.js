84cfbeb0f0988e360cac6d68c43b067842869624
samylouati <148778977+samylouati@users.noreply.github.com>
2024-02-02 13:53:43 +0100

    creation des dots

--- assets/script.js
+++ assets/script.js
@@ -19,12 +19,17 @@ const slides = [
 
 console.log(slides.length) // pour voir le nombre d'elements dans le tableau "slides" + lenght
 
+
 // elements du DOM à recupérer 
 
 let arrow_left = document.querySelector('.arrow_left'); // je recupere la fleche de gauche > sa class
 let arrow_right = document.querySelector('.arrow_right'); // je recupere la fleche de droite > sa class
 const bannerImg = document.querySelector('.banner-img'); // je recupere l'image active > sa class
 const bannerTagLine = document.querySelector('#banner p'); // je recupere le p dans l'id #banner
+
+
+
+
 
 // fonctions
 
@@ -35,10 +40,10 @@ console.log(currentSlideIndex); // console pour voir l'image en cours
 //				si l'image est la derniere, alors je vais sur la premiere
 function display(Index) {
 	if (Index < 0) {
-		currentSlideIndex = slides.lenght -1;
+		currentSlideIndex = slides.length -1 ;
 	}
 	else if (Index > slides.length -1) {
-		currentSlideIndex = 0
+		currentSlideIndex = 0 ;
 	}
 	console.log(Index) // console pour voir la nouvelle Image en cours
 	bannerImg.src = `./assets/images/slideshow/${slides[currentSlideIndex].image}`; // montre le chemin de l'image
@@ -55,8 +60,9 @@ function nextSlide () { // nom de la fonction
 //fonction : image precedente
 function previousSlide() { // nom de la fonction
 	currentSlideIndex--; // je retire -1 à l'image en cours
-	display(currentSlideIndex) // je montre l'image en cours
+	display(currentSlideIndex); // je montre l'image en cours
 }
+
 
 // evenement pour les fleches 
 
@@ -69,3 +75,20 @@ arrow_right.addEventListener('click', nextSlide); //au click sur la fleche de dr
 //function updateDots (parametre index) pour gerer le deplacement de la class dot_selected sur mes dot
 // On retire à toutes les dots la class dot_selected
 // Je prend le dot de l'index en cours et je lui met en class dot_selected
+
+// elements à créer / modifier / ajouter dans le DOM 
+
+let dot1 = document.createElement('div'); //Je créé une div "dot"
+let dot2 = document.createElement('div'); //Je créé une div "dot"
+let dot3 = document.createElement('div'); //Je créé une div "dot"
+let dot4 = document.createElement('div'); //Je créé une div "dot"
+let dots = document.querySelector('.dots'); // je recupere la balise "dots"
+dots.appendChild(dot1); //J'indique que "dots" est le parent de "dot"
+dots.appendChild(dot2); //J'indique que "dots" est le parent de "dot"
+dots.appendChild(dot3); //J'indique que "dots" est le parent de "dot"
+dots.appendChild(dot4); //J'indique que "dots" est le parent de "dot"
+dot1.classList.add('dot'); // j'applique le style "dot"
+dot1.classList.add('dot_selected'); // j'applique le style "dot_selected"
+dot2.classList.add('dot'); // j'applique le style "dot"
+dot3.classList.add('dot'); // j'applique le style "dot"
+dot4.classList.add('dot'); // j'applique le style "dot"
